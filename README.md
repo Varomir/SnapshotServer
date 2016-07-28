5b1a25c23d15009aa21e0d7089d825c4d61a2624
Vadym Feshchenko <Vadym.Feshchenko@playtech.com>
2016-07-28 11:28:18 +0300

    Add wss support (have to configure https server) or accept sertificate from the browser side

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ mvn package
 *Run*
 mws@mws-tv:/opt/SnapshotServer$ nohup java -Dport=8081 -Dsnapshot.path=./target/baseline/ -jar baseline-1.0.3.jar > serveroutput.log &
 
-ToD: build WSS https://github.com/sorelmitra/learn/tree/master/java/jetty-websocket
+Upgrade to WSS https://github.com/sorelmitra/learn/tree/master/java/jetty-websocket
--- src/main/java/com/playtech/qatools/storage/BaselineServer.java
+++ src/main/java/com/playtech/qatools/storage/BaselineServer.java
@@ -2,9 +2,9 @@ package com.playtech.qatools.storage;
 
 import com.playtech.qatools.storage.endpoints.GetBaseline;
 import com.playtech.qatools.storage.endpoints.PutBaseline;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
 import javax.websocket.server.ServerContainer;
 
@@ -12,10 +12,25 @@ public class BaselineServer {
 
     public static void main(String[] args) {
 
-        Integer port = Integer.parseInt(System.getProperty("port", "8081"));
+        Integer sslPort = Integer.parseInt(System.getProperty("port", "8081"));
         Server server = new Server();
-        ServerConnector connector = new ServerConnector(server);
-        connector.setPort(port);
+
+        SslContextFactory contextFactory = new SslContextFactory();
+        contextFactory.setKeyStorePath("./keystore");
+        contextFactory.setKeyStorePassword("changeit");
+        SslConnectionFactory sslConnectionFactory = new SslConnectionFactory(contextFactory, org.eclipse.jetty.http.HttpVersion.HTTP_1_1.toString());
+        HttpConfiguration config = new HttpConfiguration();
+        config.setSecureScheme("https");
+        config.setSecurePort(sslPort);
+        config.setOutputBufferSize(32786);
+        config.setRequestHeaderSize(8192);
+        config.setResponseHeaderSize(8192);
+        HttpConfiguration sslConfiguration = new HttpConfiguration(config);
+        sslConfiguration.addCustomizer(new SecureRequestCustomizer());
+        HttpConnectionFactory httpConnectionFactory = new HttpConnectionFactory(sslConfiguration);
+
+        ServerConnector connector = new ServerConnector(server, sslConnectionFactory, httpConnectionFactory);
+        connector.setPort(sslPort);
         server.addConnector(connector);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
